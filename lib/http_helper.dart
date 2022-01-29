0554e1f093632942fe8fdd7394c532a2409fb326
Prateek Singh <iamprateek22@gmail.com>
2022-01-30 02:16:11 +0530

    Update http_helper.dart

--- lib/http_helper.dart
+++ lib/http_helper.dart
@@ -8,7 +8,7 @@ class HttpHelper {
       'https://api.themoviedb.org/3/movie/upcoming?api_key=06a133b6db85c76f31c7c841c3a8dd5c&language=en-US';
 
   final String searchBase =
-      'https://api.themoviedb.org/3/search/movie?api_key=06a133b6db85c76f31c7c841c3a8dd5c&query=';
+      'https://api.themoviedb.org/3/search/movie?api_key=Xxxxxxxxxxxxxxxxxxx&query=';
 
   Future<List> findMovies(String title) async {
     final String query = searchBase + title;
